9c86c0756f6bb4b25d13808a13f3e9d02a48db78
Celia Manzano <celiamanzano@celiama-ltmaqvp.internal.salesforce.com>
2021-11-22 14:45:48 +0000

    5 Project

--- InitialProg/5Project/atm.js
+++ InitialProg/5Project/atm.js
@@ -1,8 +1,20 @@
+var images = [];
+images["50"] = "50.jpg";
+images["20"] = "20.jpg";
+images["10"] = "10.jpg";
 class Billete {
   constructor(v, c) {
+    this.image = new Image();
     this.valor = v;
     this.cantidad = c;
+    this.image.src = images[this.valor];
   }
+}
+
+function muestraFotoBillete(imagen) {
+  f.innerHTML += "<img src=" + imagen.src + ">";
+
+  //f.appendChild(imagen);
 }
 
 function entregarDinero() {
@@ -28,8 +40,13 @@ function entregarDinero() {
     r.innerHTML = "Lo siento no me da el dinero";
   } else {
     console.log(entregado);
-
+    // TODO: cambiar nombre a las varibles, hacer resize de las images
     for (var e of entregado) {
+      for (var i = 0; i < e.cantidad; i++) {
+        muestraFotoBillete(e.image);
+      }
+      f.innerHTML += "<br /><hr />";
+
       if (e.cantidad > 0) {
         r.innerHTML =
           r.innerHTML +
@@ -54,5 +71,6 @@ caja.push(new Billete(20, 10));
 caja.push(new Billete(10, 20));
 
 var r = document.getElementById("resultado");
+var f = document.getElementById("fotos");
 var b = document.getElementById("extraer");
 b.addEventListener("click", entregarDinero);
